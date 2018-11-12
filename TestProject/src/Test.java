2d02531a88481355830870432e7c95c312f0ee27
Padraig <Padraig@Lenovo-POGrady>
2018-11-12 17:50:06 +0000

    test

--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -1,8 +1,15 @@
 
 public class Test 
 {
+	private int test = 2;
 	public static void main(String[] args) 
 	{  
 		System.out.println("Hello World");
+	}
+	public int getTest() {
+		return test;
+	}
+	public void setTest(int test) {
+		this.test = test;
 	}
 }
